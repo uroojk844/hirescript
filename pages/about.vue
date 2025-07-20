c82ed1b9e4aa35df75083f3f1b18d6db211dfe91
Urooj Khan <uroojk844@gmail.com>
2025-07-20 21:03:24 +0530

    [ADD] adding basic stucture

--- pages/about.vue
+++ pages/about.vue
@@ -1,1 +1,3 @@
-<template>About</template>
+<template>
+    HireSript base project
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,9 @@
-<template>Hello</template>
+<script setup lang="ts">
+const users: string[] = ["Urooj", "Belal"];
+</script>
+<template>
+  <h1>Users</h1>
+  <ul>
+    <li v-for="(user, index) in users" :key="index">{{ user }}</li>
+  </ul>
+</template>
