1ba3d641f546c80f677e67c10ab250eab4119c82
Urooj Khan <uroojk844@gmail.com>
2025-07-24 19:05:06 +0530

    [ADD] adding job list and job details page

---
+++ src/components/OutlinedCard.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const { direction = "col", size = "md" } = defineProps<{
+  direction?: "col" | "row";
+  size?: "sm" | "md";
+}>();
+</script>
+
+<template>
+  <div
+    class="flex text-gray text-sm gap-2 border border-light-gray rounded-lg max-w-4xl"
+    :class="{
+      'flex-col': direction == 'col',
+      'items-center': direction == 'row',
+      'p-4': size == 'md',
+      'p-2': size == 'sm',
+    }"
+  >
+    <slot></slot>
+  </div>
+</template>
---
+++ src/components/Tag.vue
@@ -0,0 +1,5 @@
+<template>
+  <span class="inline-flex bg-gray-200 px-2 py-1 rounded w-fit text-xs capitalize">
+    <slot></slot>
+  </span>
+</template>
