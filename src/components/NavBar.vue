487cdd91404cc90c6794c2110efca3403b9e7931
Urooj Khan <uroojk844@gmail.com>
2025-07-24 15:49:04 +0530

    [ADD] adding resposive sidebar and updating home page design

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,20 +1,42 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import MaxWidth from "./MaxWidth.vue";
 import PrimaryButton from "./PrimaryButton.vue";
+import { useHandleClickOutside } from "@/composables/use-handle-click-outside";
+
+const mobileNav = ref<HTMLElement | null>(null);
+const isOpen = ref(false);
+
+useHandleClickOutside(mobileNav, isOpen);
 </script>
 
 <template>
   <nav class="py-6 xl:py-8">
     <MaxWidth class="flex items-center justify-between">
       <div class="text-2xl font-bold text-primary">HireScript</div>
-      <div class="flex gap-8 max-sm:hidden">
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/jobs">Find Jobs</RouterLink>
-        <RouterLink to="/campus">Campus Ambassador</RouterLink>
-      </div>
-      <div>
+      <div
+        ref="mobileNav"
+        class="mobile-nav sm:contents"
+        :class="{ open: isOpen }"
+      >
+        <div class="text-2xl font-bold text-primary mb-8 mobile-title">
+          HireScript
+        </div>
+
+        <div class="flex gap-8 links">
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/jobs">Find Jobs</RouterLink>
+          <RouterLink to="/campus">Campus Ambassador</RouterLink>
+        </div>
+
         <PrimaryButton>Register Now</PrimaryButton>
       </div>
+
+      <Icon
+        @click.stop="isOpen = !isOpen"
+        :icon="!isOpen ? 'uil:bars' : 'uil:times'"
+        class="text-3xl text-primary toggle-sidebar"
+      />
     </MaxWidth>
   </nav>
 </template>
@@ -27,6 +49,43 @@ a {
 
   &.router-link-exact-active {
     @apply text-primary font-medium;
+  }
+}
+
+.toggle-sidebar,
+.mobile-title {
+  display: none;
+}
+
+@media screen and (width <= 768px) {
+  .toggle-sidebar,
+  .mobile-title {
+    display: block;
+  }
+
+  .mobile-nav {
+    background-color: white;
+    position: fixed;
+    height: 100%;
+    top: 0;
+    left: 0;
+    width: 280px;
+    padding: 24px;
+    display: flex;
+    flex-flow: column;
+    transition: 150ms ease;
+    transform: translateX(-100%);
+    z-index: 99;
+
+    &.open {
+      transform: translateX(0);
+    }
+  }
+
+  .links {
+    display: flex;
+    flex-flow: column;
+    margin-bottom: auto;
   }
 }
 </style>
--- src/composables/use-salary.ts
+++ src/composables/use-salary.ts
@@ -1,3 +1,10 @@
+/**
+ * This will convert numeric value into salary format.
+ * @param value to be converted
+ * @param display default compact 12,345 -> 12K
+ * @returns 
+ */
+
 export function useSalary(
   value: number,
   display: "compact" | "standard" = "compact"
