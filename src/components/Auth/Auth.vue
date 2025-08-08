3baa6b37679b2c81b54f6c9081ebd3836bec292c
Mohd Belal Naim <mohd.belal.naim@sap.com>
2025-08-08 22:45:01 +0530

    [NEW] Added auth

---
+++ src/components/Auth/Auth.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue';
+import Login from './Login.vue';
+import Signup from './Signup.vue';
+import { onMounted, provide, ref, watch } from 'vue';
+import ForgotPassword from './ForgotPassword.vue';
+
+const current = ref<"Login" | "Signup" | "Forgot" | "null">("Login");
+provide('authCurrent', current);
+function updateBodyOverflow(value: string) {
+    document.body.style.overflow = value !== "null" ? "hidden" : "auto";
+}
+
+watch(current, (value) => {
+    updateBodyOverflow(value);
+});
+
+onMounted(() => {
+    updateBodyOverflow(current.value);
+});
+
+</script>
+
+<template>
+    <div class="inset-0 fixed bg-black/50 grid place-items-center z-50">
+        <div
+            class="w-[90%] sm:w-[80%] md:w-[60%] lg:w-[40%] xl:w-[26%] p-6 sm:p-8 md:p-10 lg:p-12 rounded-4xl bg-white relative">
+            <Icon icon="clarity:times-line"
+                class="text-2xl absolute right-4 top-4 sm:right-5 sm:top-5 md:right-6 md:top-6 cursor-pointer" />
+            <Login v-if="current === 'Login'" />
+            <Signup v-else-if="current === 'Signup'" />
+            <ForgotPassword v-else />
+        </div>
+    </div>
+</template>
