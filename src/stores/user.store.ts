4dd61bfccfe8e9db75303d91167f1406a1c34869
Mohd Adnan Sheikh <adnansheikh3721@gmail.com>
2025-08-09 22:19:06 +0530

    [ADD] Auth is implemented

--- src/stores/user.store.ts
+++ src/stores/user.store.ts
@@ -1,37 +1,38 @@
 import { defineStore } from "pinia";
 import type { IUser } from "@/interface/user.interface.ts";
-import { signOut ,  } from "firebase/auth";
+import { signOut, } from "firebase/auth";
 import { auth } from "@/firebase/config";
 // import { useRouter } from "vue-router";
 import { router } from "@/router";
 import { nextTick } from "vue";
 
 export const useUserStore = defineStore("user", {
-  state: () => ({
-    user: null as IUser | null,
-  }),
-  getters:{
-    getUser(state) {
-      return state.user;
+    state: () => ({
+        user: null as IUser | null,
+    }),
+    getters: {
+        getUser(state) {
+            return state.user;
+        },
     },
-  },
-  actions: {
-    setUser(user: IUser) {
-      this.user = user;
+    actions: {
+        setUser(user: IUser) {
+            this.user = user;
+        },
+
+        async handleLogout() {
+            try {
+                await nextTick(); // Ensure the DOM is updated before proceeding
+                await signOut(auth);
+                this.user = null;
+
+                router.push("/"); // Redirect to home after logout
+                document.body.style.overflow = "auto";
+
+            } catch (error) {
+                console.error("Error logging out:", error);
+            }
+        }
     },
-
-    async handleLogout() {
-  try {
-    await nextTick(); // Ensure the DOM is updated before proceeding
-    await signOut(auth);
-    this.user = null; 
-    router.push("/"); // Redirect to home after logout
-    document.body.style.overflow = "auto";
-
-  } catch (error) {
-    console.error("Error logging out:", error);
-  }
-}
-  },
-  persist:true
+    persist: true
 });
